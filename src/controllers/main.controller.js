edf7c725d9df404d30b5716e4563a48880414ca0
Jakaran <80308625+jaskaranproductions@users.noreply.github.com>
2022-01-18 17:38:07 +0530

    c

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -1,11 +1,23 @@
 const express = require("express");
 
 const router = express.Router();
+let data = [
+  {
+    name: "dd",
+    price: 300,
+    rate: 3,
+  },
+  {
+    name: "ddsadsa",
+    price: 3040,
+    rate: 4,
+  },
+];
 
 router.get("", (req, res) => {
   try {
     res.render("index", {
-      name: "jaskaran",
+      name: data,
     });
   } catch (err) {
     console.log(err);
