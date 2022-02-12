6a3871986219217eb551a5d43ce3e3d70afe796d
Jakaran <80308625+jaskaranproductions@users.noreply.github.com>
2022-02-12 18:41:46 +0530

    deploy

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use("/comment", commentController);
 app.use("/checkout", (req, res) => res.render("checkout"));
 app.use("/payment", (req, res) => res.render("payment"));
 
-app.listen(5000, async () => {
+app.listen(3000, async () => {
   await require("./src/configs/dp")();
-  console.log("listning on port 5000");
+  console.log("listning on port 3000");
 });
